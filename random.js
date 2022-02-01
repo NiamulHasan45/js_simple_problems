f47882066ab3d265e8f558d4bfec432931bf10dd
Niamul.Hasan45 <niamul.rony@gmail.com>
2022-02-01 10:26:50 +0600

    first lectures

--- random.js
+++ random.js
@@ -3,3 +3,8 @@ for(let n = 0; n<20; n++){
     const rNumber = Math.round(number);
     console.log(rNumber);
 }
+
+const fNumber = 12.456;
+console.log(Math.ceil(fNumber));
+console.log(Math.floor(fNumber));
+console.log(Math.round(fNumber));
